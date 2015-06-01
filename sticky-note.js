a4711dce46a01b989b6f46e2261d1af8de632fd6
Juliet Brown <apprentice@StMac4.local>
2015-06-01 12:25:20 +1200

    added board box

---
+++ sticky-note.js
@@ -0,0 +1,32 @@
+
+
+function makeNote (e) {
+
+        if (e.eventPhase === 2) {
+
+
+             var postit = $('<div class="ui-widget-content newbox" style="top:' + e.pageY + 'px; left: ' + e.pageX + 'px;"><span id="close">Delete comment</span><p>Your comment:</p><textarea></textarea></div>').draggable();
+           $('#board').append(postit);
+
+       }
+
+}
+
+function deleteNote() {
+    $(this).parent('#newbox').remove();
+}
+
+
+$(document).ready(function(){
+
+
+    $('#board').click(function(e){
+        makeNote(e);
+    });
+
+
+    $("#close").click(function () {
+        deleteNote();
+    });
+
+});
